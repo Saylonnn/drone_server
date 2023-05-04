c53f366674b7668eea7526d59f3b6b7a84b5cca4
saylonn <spacexd@gmx.de>
2023-05-04 15:03:28 +0200

    fix

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const authDrone = require('./middleware/authDrone');
 const https = require('https');
 const fs = require('fs');
 const helmet = require('helmet');
+const WebSocket = require('ws');
 
 
 
@@ -19,7 +20,8 @@ app.use(express.json());
 const PORT = 5000;
 
 
-
+const sessions = [];
+const streamOnline = false;
 
 
 app.get('/' , (req, res)=>{
@@ -50,11 +52,56 @@ app.get('/drone/authDrone', authDrone, (req, res) => {
     console.log("Drone Authenticaiton finished");
 });
 
+app.post('(/drone/startTransmission', authDrone,  (req, res) => {
+    const session = {
+        id : Math.random().toString(36).substring(7),
+        broadcaster: null,
+        viewers: []
+    };
+    sessions.push(session);
+    if (!streamOnline) {
+        const wss = new WebSocket.Server({ port: 8080});
+        // on connect
+        wss.on('connection', (ws) => {
+            if(!session.broadcaster) {
+                session.broadcaster = ws;
+                ws.send(JSON.stringify({type: 'broadcaster'}));
+            } else {
+                session.viewers.push(ws);
+                ws.send(JSON.stringify({type: 'viewer', sessionID: session.id}));
+                session.broadcaster.send(JSON.stringify({type:'viewerConnected', viewerCount: session.viewers.length }));
 
+            }
+            // on message
+            ws.on('message', (message) => {
+                session.viewers.forEach((viewer) => {
+                    viewer.send(message);
+                });
+            });
 
-app.listen(
-    PORT,
-    () => console.log(`its alive on http://localhost:${PORT}`)
-)
+            // on close connection
+            ws.on('close', () => {
+                if (ws === session.broadcaster) {
+                    session.viewers.forEach((viewer) => {
+                        viewer.close();
+                    });
+                    session.splice(session.indexOf(session), 1);
+                } else {
+                    session.viewers.splice(session.viewers.indexOf(ws), 1);
+                    session.broadcaster.send(JSON.stringify({type: 'viewerDisconnected', viewerCount: session.viewers.length}));
+
+                }
+            });
+        })
+    }
+});
+
+const server = app.listen(PORT, () => console.log(`its alive on http://localhost:${PORT}`));
+
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws);
+    });
+});
 
 
